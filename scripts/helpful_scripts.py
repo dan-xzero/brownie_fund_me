85d2327750594fd8780025be60a7dfb4741a2e80
Dan_xzero <danish.alam58@gmail.com>
2023-08-09 00:44:25 +0530

    Test on mainnet failed

--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -1,13 +1,17 @@
 from brownie import MockV3Aggregator, network, config, accounts
 from web3 import Web3
 
+FORKED_LOCAL_ENVIRONMENT = ["mainnet-fork-dev", "mainnet-fork"]
 LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local"]
 DECIMALS = 8
 STARTING_PRICE = 200000000000
 
 
 def get_account():
-    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+    if (
+        network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS
+        or network.show_active() in FORKED_LOCAL_ENVIRONMENT
+    ):
         return accounts[0]
     else:
         return accounts.add(config["wallets"]["from_key"])
