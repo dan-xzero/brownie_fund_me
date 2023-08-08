3b0bce11ad99a39b877227918dabb2f107929963
Dan_xzero <danish.alam58@gmail.com>
2023-08-08 23:27:14 +0530

    test file

--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -2,8 +2,8 @@ from brownie import MockV3Aggregator, network, config, accounts
 from web3 import Web3
 
 LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local"]
-DECIMALS = 18
-STARTING_PRICE = 2000
+DECIMALS = 8
+STARTING_PRICE = 200000000000
 
 
 def get_account():
@@ -17,7 +17,5 @@ def deploy_mocks():
     print(f"The active network is {network.show_active()}")
     print("Deploying Mocks...")
     if len(MockV3Aggregator) <= 0:
-        MockV3Aggregator.deploy(
-            DECIMALS, Web3.toWei(STARTING_PRICE, "ether"), {"from": get_account()}
-        )
+        MockV3Aggregator.deploy(DECIMALS, STARTING_PRICE, {"from": get_account()})
         print("Mocks Deployed")
